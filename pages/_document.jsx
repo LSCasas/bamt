07075060cb869cce64069e4b1e5ddc607a3c0978
Luis Casas <luisscasas45@gmail.com>
2025-03-19 00:11:56 -0600

    FEAT/ add favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+          <link
+            rel="icon"
+            href="/icons/icon-512x512.png"
+            type="image/png"
+            sizes="512x512"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
